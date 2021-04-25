213171f898ce5bc6a99d9e90cdec499882389d71
samjoshuaberchmans <bsamjosh@icloud.com>
2021-04-25 02:13:42 -0400

    Added the PrintBST Function. Referred GeeksFor Geeks.
    https://www.geeksforgeeks.org/print-binary-tree-2-dimensions/

--- src/binarySearchTree/BST.java
+++ src/binarySearchTree/BST.java
@@ -1,7 +1,8 @@
 package binarySearchTree;
 
 class BST {
-	
+
+	static final int COUNT = 0;
 	public Node createNewNode(int k){
 		Node a = new Node();
 		a.data = k;
@@ -31,8 +32,26 @@ class BST {
 		System.out.println(node.data+ " ");
 		printBSTInorder(node.right);
 	}
+
+	public void print2dUtil(Node node , int space){
+		if( node == null)
+			return;
+
+		space += COUNT;
+
+		print2dUtil(node.right , space);
+		System.out.println("\n");
+		for (int i = 0 ; i < space ;  i++){
+			System.out.println(" ");
+		}
+		System.out.println(node.data);
+
+		print2dUtil(node.left , space);
+	}
 	
 	public void printBST(Node node){
+		print2dUtil(node,0);
+
 	}
 	
 	public boolean isBST(Node node , Node left , Node right){
