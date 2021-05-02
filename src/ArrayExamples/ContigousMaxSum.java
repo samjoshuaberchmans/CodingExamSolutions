73cdab8aad8d0b18263196e463de16992776dbaa
samjoshuaberchmans <bsamjosh@icloud.com>
2021-05-02 01:53:16 -0400

    Max sum of contigous number Added a new test cases .

--- src/ArrayExamples/ContigousMaxSum.java
+++ src/ArrayExamples/ContigousMaxSum.java
@@ -21,9 +21,11 @@ public class ContigousMaxSum {
 
         int[] in1 = {1,3,-10,20}; // Expected 20
         int[] in2 = {1,3,-10,2}; //Expected 4
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
 
         System.out.println("int 1 - " + maxSubArray(in1));
         System.out.println("int 2 - " + maxSubArray(in2));
+        System.out.println("int 2 - " + maxSubArray(nums));
 //        List<Integer> inList1 = Arrays.asList(in1);
 //        List<Integer> inList2 = Arrays.asList(in2);
 
