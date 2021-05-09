4522cacb3a3ddbe4d50d6fb6e4dd9ac0c8c9b647
samjoshuaberchmans <bsamjosh@icloud.com>
2021-05-08 23:04:38 -0400

    Modified the code to contain my code and algo expert code. Fixed the issue related to null pointer issue.

--- src/AEArrays/TaskAssignment.java
+++ src/AEArrays/TaskAssignment.java
@@ -1,7 +1,6 @@
 package AEArrays;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 
@@ -10,6 +9,7 @@ public class TaskAssignment {
     public static HashMap<Integer,ArrayList<Integer>> timeToIndexsMap;
 
     public static void FillTimeToIndexMap(ArrayList<Integer> inList){
+        timeToIndexsMap = new HashMap<>();
         for(int i = 0 ; i < inList.size() ; i++){
             if(timeToIndexsMap.containsKey(inList.get(i))){
                 ArrayList<Integer> temp = timeToIndexsMap.get(inList.get(i));
@@ -49,17 +49,55 @@ public class TaskAssignment {
     public static ArrayList<ArrayList<Integer>> AssignTask(int k , ArrayList<Integer> tasks){
         FillTimeToIndexMap(tasks);
         Collections.sort(tasks);
-        ArrayList<Integer> temp = new ArrayList<>();
         ArrayList<ArrayList<Integer>> result = new ArrayList<>();
         int inListSize = tasks.size();
 
         for(int i = 0 ; i < inListSize/2;i++){
 //            System.out.println("i - " + i + " - listSize index - " + (inListSize-i-1));
+            ArrayList<Integer> temp = new ArrayList<>();
             temp.add(returnIndexForValueFromHashMap(Integer.valueOf(tasks.get(i)),0));
-            temp.add(returnIndexForValueFromHashMap(Integer.valueOf(tasks.get(i)),1));
+            temp.add(returnIndexForValueFromHashMap(Integer.valueOf(tasks.get(inListSize-i-1)),1));
             result.add(temp);
         }
         return result;
+    }
+
+//     From Algo Expoert
+    public static HashMap<Integer, ArrayList<Integer>> getTaskDurationsToIndices(ArrayList<Integer> tasks) {
+        HashMap<Integer, ArrayList<Integer>> taskDurationsToIndices =
+        new HashMap<Integer, ArrayList<Integer>>();
+        for (int idx = 0; idx < tasks.size(); idx++) {
+            int taskDuration = tasks.get(idx);
+            if (taskDurationsToIndices.containsKey(taskDuration)) {
+                taskDurationsToIndices.get(taskDuration).add(idx);
+            } else {
+                ArrayList<Integer> temp = new ArrayList<Integer>();
+                temp.add(idx);
+                taskDurationsToIndices.put(taskDuration, temp);
+            }
+        }
+        return taskDurationsToIndices;
+    }
+
+    public static ArrayList<ArrayList<Integer>> taskAssignment(int k, ArrayList<Integer> tasks) {
+        ArrayList<ArrayList<Integer>> pairedTasks = new ArrayList<ArrayList<Integer>>();
+        HashMap<Integer, ArrayList<Integer>> taskDurationsToIndices = getTaskDurationsToIndices(tasks);
+        ArrayList<Integer> sortedTasks = tasks;
+        Collections.sort(sortedTasks);
+        for (int idx = 0; idx < k; idx++) {
+            int task1Duration = sortedTasks.get(idx);
+            ArrayList<Integer> indicesWithTask1Duration = taskDurationsToIndices.get(task1Duration);
+            int task1Index = indicesWithTask1Duration.remove(indicesWithTask1Duration.size() - 1);
+            int task2SortedIndex = tasks.size() - 1 - idx;
+            int task2Duration = sortedTasks.get(task2SortedIndex);
+            ArrayList<Integer> indicesWithTask2Duration = taskDurationsToIndices.get(task2Duration);
+            int task2Index = indicesWithTask2Duration.remove(indicesWithTask2Duration.size() - 1);
+            ArrayList<Integer> pairedTask = new ArrayList<Integer>();
+            pairedTask.add(task1Index);
+            pairedTask.add(task2Index);
+            pairedTasks.add(pairedTask);
+        }
+        return pairedTasks;
     }
 
     public static void main(String[] args){
@@ -71,6 +109,7 @@ public class TaskAssignment {
         }
         ArrayList<ArrayList<Integer>> output = AssignTask(k,inList);
         System.out.println(output);
-
+        ArrayList<ArrayList<Integer>> output1 = taskAssignment(k,inList);
+        System.out.println(output1);
     }
 }
